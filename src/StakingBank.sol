19f74569de537037fd9938462eff2b9f0ea31006
infamousdegen <github@infamousdegen.me>
2023-02-28 14:49:43 +0530

    Update2

--- src/StakingBank.sol
+++ src/StakingBank.sol
@@ -11,89 +11,173 @@ import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract Staking is ERC4626,Ownable2Step{
+    using Math for uint256;
+
+    IERC20 private immutable baseToken;
 
     //@todo I can reduce this to a small time unit
     uint64 immutable public timeConstant;
     //@note time when the contract was deployed
     uint64 immutable public deploymentTime;
-
     uint32 constant public decayingFactor1 = 2e3; //20% in basis points
-    uint32 constant public decayingFactor2 = 3e3; //30% in basis points
-    uint32 constant public decayingFactor3 = 5e3; //50% in basis points
-    uint32 constant denominator =  1e4; // 1,0000 to be used as denominator
+    uint32 constant public decayingFactor2 = 5e3; //50% in basis points(20%+30%)
+    uint32 constant public decayingFactor3 = 1e4; //100% in basis points(20%+30%+50%)
+
+    uint256 constant denominator =  1e4; // 1,0000 to be used as denominator
+    
 
     uint256 public rewardAmount;
 
-    //Snapshot Of Deposit Time
-    mapping(address => uint64) public snapshot;
+    //tracking totalDeposit seperately to prevent inflation attack 
+    uint256 public totalDeposit;
+
 
     //Errors
 
     error timePassed();
     error withdrawLocked();
 
-//@note: using inline check saves gas compared to modifier but including this for readability
+
+//@Note: Gas efficient to call this inline but including it as a modifier for reaadeability
     modifier checkWithdraw() {
         //@audit test this 
         if(uint64(block.timestamp + timeConstant) > uint64(block.timestamp) && uint64(block.timestamp) < uint64(block.timestamp + (2 * timeConstant))) revert withdrawLocked();
         _;
     }
 
+//@Note : Its gas efficient to call this inline but including it as a modifier for readeability
+    modifier poke() {
+        syncRewards();
+        _;
+    }
+
     
 
     //@note Enter the time constant in second eg: 1 day = 86400 seconds
-    constructor(IERC20 _baseToken,uint64 _timeConstant,uint256 _rewardAmotunt) ERC4626( _baseToken) ERC20("Staking","STK"){
+    constructor(IERC20 _baseToken,uint64 _timeConstant) ERC4626( _baseToken) ERC20("Staking","STK"){
+        baseToken = _baseToken;
+
         timeConstant = _timeConstant;
         deploymentTime = uint64(block.timestamp);
-        rewardAmount = _rewardAmotunt;
 
     }
 
 
-
+        /* -------------------------------------------------------------------
+       |                      Public functions                               |
+       | ________________________________________________________________ | */
     function deposit(uint256 assets, address receiver) public override returns(uint256){
-        if(uint64(deploymentTime + timeConstant) < uint64(block.timestamp)) revert timePassed();
-
-        snapshot[receiver] = uint64(block.timestamp);
-        
+        if(uint64(deploymentTime + timeConstant) < uint64(block.timestamp)) revert timePassed();       
         return(super.deposit(assets,receiver));
     }
+
+
 
     function mint(uint256 shares, address receiver) public override returns (uint256){
 
         if(uint64(deploymentTime + timeConstant) < uint64(block.timestamp)) revert timePassed();
-        snapshot[receiver] = uint64(block.timestamp);
+
         return(super.mint(shares,receiver));
     }
 
-//@audit test these
-    // function withdraw(uint256 assets,address receiver) public  override checkWithdraw returns(uint256){
-    //     super.withdraw(assets,receiver,owner());
-    // }
+    //@Note: wont' break the interface because the reward amount is not minted as share (previewWithdraw)
 
-    //@note:Updating the convertoAssets internal function
-    //@note: The actual withdraw or redeem will cal convertToAsset
+    function withdraw(uint256 assets,address receiver,address owner) public override checkWithdraw poke returns(uint256){
+
+            return(super.withdraw(assets,receiver,owner));
+    }
+
+    //@Note: wont' break the interface because the reward amount is not minted as share (previewRedeem)
+    function redeem(uint256 shares,address receiver,address owner) public override checkWithdraw poke returns (uint256){
+        return(super.redeem(shares,receiver,owner));
+    }
 
 
-    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view override returns (uint256 shares){
+       //@note: Direct of tokens transfer to the the contract will be considered for rewards
+
+       //@note: syncrewards has to called manually after transferring the tokens
+    function syncRewards() public {
+        rewardAmount = baseToken.balanceOf(address(this)) - totalDeposit;
+
+    }
+
+
+        /* -------------------------------------------------------------------
+       |                      View functions                               |
+       | ________________________________________________________________ | */
+    function calculateRewards(address _depositor,Math.Rounding rounding) public view returns(uint256 rewards){
         uint256 supply = totalSupply();
-        uint256 totalAsset = totalAssets() - rewardAmount;
+        uint256 depositorShares = balanceOf(_depositor);
+        uint256 factor;
+        
+        if(uint64(block.timestamp) > uint64(deploymentTime+(2 * timeConstant))){
+            if(uint64(block.timestamp) > uint64(deploymentTime + (3 * timeConstant))){
+                if(uint64(block.timestamp) > uint64(deploymentTime + (4 * timeConstant))){
+                    factor = decayingFactor3;
+                }
+                else{
+                    factor = decayingFactor2;
+                }
+            }
+            else{
+                factor = decayingFactor1;
+            }
+        }
+
+        uint256 _denominator = decimals() * denominator;
+        uint256 rewardPool = rewardAmount.mulDiv(factor,_denominator,rounding);
+
+        return
+            (supply == 0 || depositorShares == 0)
+            ? 0
+            : depositorShares.mulDiv(rewardPool,supply,rounding);
+    }
+
+
+
+        //@Note Overriding to change totalAsset to totalDeposit as reward amount should not be calculated as stake
+
+    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view override returns (uint256 shares) {
+        uint256 supply = totalSupply();
         return
             (assets == 0 || supply == 0)
                 ? _initialConvertToShares(assets, rounding)
-                : assets.mulDiv(supply, totalAsset, rounding);
+                : assets.mulDiv(supply, totalDeposit, rounding);
     }
+
+    //@Note Overriding to change totalAsset to totalDeposit as reward amount should not be calculated as stake
+    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view override returns (uint256 assets) {
+        uint256 supply = totalSupply();
+        return
+            (supply == 0) ? _initialConvertToAssets(shares, rounding) : shares.mulDiv(totalDeposit, supply, rounding);
     }
 
 
-    function _convertToAssets(address _owner,uint256 shares, Math.Rounding rounding) internal view override returns (uint256 assets){
-        uint64 depositTime = snapshot[_owner];
+
+
+        /* -------------------------------------------------------------------
+       |                      Core functions                               |
+       | ________________________________________________________________ | */
 
 
 
+    function _withdraw(address caller,address receiver,address owner,uint256 assets,uint256 shares) internal override{
+        totalDeposit = totalDeposit - assets;
+        assets = assets + calculateRewards(owner,Math.Rounding.Down);
 
+        super._withdraw(caller,receiver,owner,assets,shares);
     }
-    
+
+    //@Note: Updating Total Deposit inside here because both deposit and mint will call this function in the end
+    function _deposit(address caller,address receiver,uint256 assets,uint256 shares) internal override{
+            totalDeposit = totalDeposit + assets;
+            super._deposit(caller,receiver,assets,shares);
+    }
+
+
+
+
+
 
 
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,66 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/StakingBank.sol";
+import "lib/openzeppelin-contracts/contracts/mocks/ERC20Mock.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    Staking public staking;
+    ERC20Mock public mock;
+
+    address public owner = address(0x1337);
+
+    address public depositor1 = address(0x69420);
+    
+    address public depositor2 = address(0x5544);
+
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        vm.label(owner,"Owner");
+        vm.label(depositor1,"user1");
+        vm.label(depositor2,"user2");
+        vm.startPrank(owner);
+
+        mock = new ERC20Mock("MOCK","MCK",owner,1000 * 10**18);
+
+        mock.mint(depositor1, 1000 *10**18);
+        mock.mint(depositor2, 4000 * 10**18);
+
+        staking = new Staking(mock,0);
+        mock.transfer(address(staking), 1000 * 10**18);
+        vm.stopPrank();
+
+        vm.prank(depositor1);
+        mock.approve(address(staking), type(uint256).max);
+
+        vm.prank(depositor2);
+        mock.approve(address(staking), type(uint256).max);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+
+
+    /**  @dev  Test first depositor shareBalance*/
+    function test_deposit_depositor1() public {
+        vm.startPrank(depositor1);
+        uint256 shares = staking.deposit(1000 *10**18, depositor1);
+        vm.stopPrank();
+        assertEq(staking.balanceOf(depositor1),shares);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    /**  @dev  Test second depositor shareBalance*/
+    function test_deposit_depositor2() public {
+
+        vm.startPrank(depositor2);
+        uint256 shares = staking.deposit(4000 *10**18, depositor2);
+        vm.stopPrank();
+        assertEq(staking.balanceOf(depositor2),shares);
+
+    }
+
+
+/**  @dev  Test whether the sync rewards is performed correctly*/
+    function test_rewardAmount() public {
+        staking.syncRewards();
+        assertEq(staking.rewardAmount(),1000 * 10**18);
     }
 }
